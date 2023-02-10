c391e963a32e6c42fa7492ecb8058faada2c7f63
Rob Eastwood <robeastwood@gmail.com>
2023-02-10 02:56:53 +0000

    holding page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,8 @@
-import './bootstrap';
+import "./bootstrap";
+import "../css/app.css";
 
-import Alpine from 'alpinejs';
-import focus from '@alpinejs/focus';
+import Alpine from "alpinejs";
+import focus from "@alpinejs/focus";
 window.Alpine = Alpine;
 
 Alpine.plugin(focus);
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: "localhost",
+        },
+    },
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: [
-                ...refreshPaths,
-                'app/Http/Livewire/**',
-            ],
+            input: ["resources/js/app.js"],
+            refresh: [...refreshPaths, "app/Http/Livewire/**"],
         }),
     ],
 });
