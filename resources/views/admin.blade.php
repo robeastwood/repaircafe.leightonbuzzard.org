4ddbbc0f8cbd6761c95326dd57ac72b455a62789
Rob Eastwood <robeastwood@gmail.com>
2023-02-21 00:13:43 +0000

    placeholder table in admin

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,10 +6,83 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
+        <div class="max-w-7xl mx-auto sm:px-6 md:px-8">
+
+
+            <section class="px-4 mx-auto">
+                <h2 class="text-lg font-medium text-gray-800 dark:text-white">Users</h2>
+                <p class="mt-1 text-sm text-gray-500 dark:text-gray-300">Registered Users</p>
+                <div class="flex flex-col mt-6">
+                    <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                            <div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+                                <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                                    <thead class="bg-gray-50 dark:bg-gray-800">
+                                        <tr>
+                                            <th scope="col"
+                                                class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                Name & Email
+                                            </th>
+
+                                            <th scope="col"
+                                                class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                Status
+                                            </th>
+
+                                            <th scope="col"
+                                                class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                Activity
+                                            </th>
+
+                                            <th scope="col" class="relative py-3.5 px-4">
+                                                <span class="sr-only">Manage</span>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody
+                                        class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                                        <tr>
+                                            <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
+                                                <div>
+                                                    <h2 class="font-medium text-gray-800 dark:text-white ">name</h2>
+                                                    <p class="text-sm font-normal text-gray-600 dark:text-gray-400">
+                                                        email
+                                                    </p>
+                                                </div>
+                                            </td>
+                                            <td class="px-12 py-4 text-sm font-medium whitespace-nowrap">
+                                                <div
+                                                    class="inline px-3 py-1 text-sm font-normal rounded-full text-emerald-500 gap-x-2 bg-emerald-100/60 dark:bg-gray-800">
+                                                    Guest
+                                                </div>
+                                            </td>
+                                            <td class="px-4 py-4 text-sm whitespace-nowrap">
+                                                <div>
+                                                    <p class="text-gray-500 dark:text-gray-400">Items: 4</p>
+                                                    <h4 class="text-gray-700 dark:text-gray-200">Fixes: 10</h4>
+                                                </div>
+                                            </td>
+                                            <td class="px-4 py-4 text-sm whitespace-nowrap text-right">
+                                                <button
+                                                    class="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg dark:text-gray-300 hover:bg-gray-100">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                        class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+                                                    </svg>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
         </div>
     </div>
 </x-app-layout>
