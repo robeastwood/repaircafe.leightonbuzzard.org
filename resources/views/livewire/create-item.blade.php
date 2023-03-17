e19861336a5798458545d2f8139a9f675aa14d1a
Rob Eastwood <robeastwood@gmail.com>
2023-03-17 22:17:12 +0000

    fix typos in device form

--- resources/views/livewire/create-item.blade.php
+++ resources/views/livewire/create-item.blade.php
@@ -24,7 +24,7 @@
 
             <div class="mt-3">
                 <label class="block font-medium text-sm text-gray-700" for="description">
-                    Device Description
+                    Item Description
                     <p class="text-xs text-gray-500">
                         What is this item? Can you provide the make/model/year?
                     </p>
@@ -41,7 +41,7 @@
                 <label class="block font-medium text-sm text-gray-700" for="issue">
                     Issue / Symptoms
                     <p class="text-xs text-gray-500">
-                        Please describe in as much detal as you can what the problem is
+                        Please describe in as much detail as you can what the problem is
                     </p>
                     @error('issue')
                         <span class="text-sm text-red-800">{{ $message }}</span>
@@ -70,7 +70,7 @@
             </div>
 
             <p class="text-sm text-gray-600 mt-6">
-                By adding your device, you are agreeing to our <a href="/repair-policy" target="_blank"
+                By adding your item, you are agreeing to our <a href="/repair-policy" target="_blank"
                     rel="noopener noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline">Repair Policy</a>
                 and understand that your item will only be fixed on a best-efforts basis, subject to assessment and
                 availablity, and accept the risks involved in any agreed repair process.
